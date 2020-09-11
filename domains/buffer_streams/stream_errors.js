3870a845af9c23709478108bcbcc08c019d8d59d
Juan Carlos Blanco <juan.blanco.ornelas@ibm.com>
2020-09-11 12:12:39 -0500

    Create stream_errors.js

---
+++ domains/buffer_streams/stream_errors.js
@@ -0,0 +1,21 @@
+const { PassThrough } = require('stream');
+
+const passThrough = new PassThrough();
+
+passThrough.on('error', (err) => {
+  console.log('passThrough encountered an error', err);
+});
+
+process.stdin.on('error', (err) => {
+  console.log('Stdin encountered an error', err);
+});
+
+process.stdout.on('error', err => {
+  console.log('Stdout encountered an error', err);
+});
+
+process.stdin.pipe(passThrough).pipe(process.stdout);
+
+passThrough.emit('error', new Error('Whoops!'));
+process.stdout.emit('error', new Error('My bad :('));
+process.stdin.emit('error', new Error('Better next time mate...'));
