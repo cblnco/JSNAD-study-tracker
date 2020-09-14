37884db44cedeecfe88db44d04ace7de744cd7b4
Juan Carlos Blanco <juan.blanco.ornelas@ibm.com>
2020-09-14 18:15:44 -0500

    Added pipe chains example.

---
+++ domains/buffer_streams/pipe_chain.js
@@ -0,0 +1,22 @@
+// Example to demo pipe chains.
+const fs = require('fs');
+const { Transform } = require('stream');
+
+const allCaps = new Transform({
+  transform: function(chunk, econding, callback) {
+    callback(null, chunk.toString().toUpperCase());
+  }
+});
+
+const removeSpace = new Transform({
+  transform: function(chunk, encoding, callback) {
+    callback(null, chunk.toString().replace(/\s/g, ''));
+  }
+});
+
+const readFile = fs.createReadStream('./lorem.txt');
+
+readFile
+  .pipe(allCaps)
+  .pipe(removeSpace)
+  .pipe(process.stdout);
