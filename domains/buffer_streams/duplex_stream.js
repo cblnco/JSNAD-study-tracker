99f5d2ac735d36cba63c96910cf4486946bf7325
Juan Carlos Blanco <juan.blanco.ornelas@ibm.com>
2020-09-15 12:26:49 -0500

    Added duplex_stream.js file.

---
+++ domains/buffer_streams/duplex_stream.js
@@ -0,0 +1,22 @@
+// Duplex stream example, it will output letters from A to Z and echo the received inputs from command line.
+const { Duplex } = require('stream');
+
+const duplexStream = new Duplex({
+    read(size) {
+        this.push(String.fromCharCode(this.currentChar));
+        this.currentChar += 1;
+
+        if (this.currentChar > 90) {
+            this.push(null);
+        }
+    },
+
+    write(chunk, encoding, callback) {
+        console.log('Received chunk:', chunk.toString());
+        callback();
+    }
+});
+
+duplexStream.currentChar = 65;
+
+process.stdin.pipe(duplexStream).pipe(process.stdout);
