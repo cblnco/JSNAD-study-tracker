3272b20f6c9c8f8aeb0525d2c9ee2cd3083e1cc1
Juan Carlos Blanco <juan.blanco.ornelas@ibm.com>
2020-09-23 12:11:56 -0500

    Added from2-to2-examples directory.

---
+++ domains/buffer_streams/from2-to2-examples/readable_stream.js
@@ -0,0 +1,12 @@
+const from2 = require('from2');
+const to2 = require('to2');
+
+const readableStream = from2(() => {
+    readableStream.push(Buffer.from('Hello World'));
+    readableStream.push(null);
+});
+
+readableStream.on('data', (data) => {
+    console.log(data);
+    console.log(data.toString());
+});
