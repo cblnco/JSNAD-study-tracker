e6d0993e32b94b4890fb524cdc67500237016b62
Juan Carlos Blanco <juan.blanco.ornelas@ibm.com>
2020-09-24 18:23:03 -0500

    Added buffer_methods.js example.

---
+++ domains/buffer_streams/buffer_methods.js
@@ -0,0 +1,26 @@
+const buffer = Buffer.from('Hey there mate', 'utf-8');
+
+let string = '';
+for (const value of buffer) {
+    string += String.fromCharCode(value);
+}
+console.log(`Value using buffer in a for...of sentence: ${string}`);
+
+string = '';
+for (const value of buffer.values()) {
+    string += String.fromCharCode(value);
+}
+console.log(`Value of buffer calling buffer.values() in a for...of sentence ${string}`);
+
+let array = [];
+for (const key of buffer.keys()) {
+    array.push(key);
+}
+console.log(`buffer.keys() array: [${array.join(',')}]`);
+
+array = [];
+for (const value of buffer.entries()) {
+    array.push(value);
+}
+console.log('buffer.entries()');
+console.log(JSON.stringify(array.map(([key, value]) => [key, String.fromCharCode(value)]), null, 4));
