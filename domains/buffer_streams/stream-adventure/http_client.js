03d9ed87b7157046163c13fcec442fe604b9931b
Juan Carlos Blanco <juan.blanco.ornelas@ibm.com>
2020-09-25 14:03:05 -0500

    Updated stream-adventure exercise #10 - HTTP CLIENT.

---
+++ domains/buffer_streams/stream-adventure/http_client.js
@@ -0,0 +1,9 @@
+const { request } = require('http');
+
+const options = {
+    method: 'POST'
+};
+
+const req = request('http://localhost:8099', options, (res) => res.pipe(process.stdout));
+
+process.stdin.pipe(req);
