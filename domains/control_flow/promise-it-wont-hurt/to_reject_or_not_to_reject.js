881fbebcb63050b68c0695a2ebe4095d9da3f914
Juan Carlos Blanco <juan.blanco.ornelas@ibm.com>
2020-09-30 17:52:04 -0500

    Completed promise-it-wont-hurt exercise #4 - TO REJECT OR NOT TO REJECT.

---
+++ domains/control_flow/promise-it-wont-hurt/to_reject_or_not_to_reject.js
@@ -0,0 +1,10 @@
+const promise = new Promise((resolve, reject) => {
+   resolve('I FIRED');
+   reject(new Error('I DID NOT FIRE'));
+});
+
+function onRejected(error) {
+    console.log(error.message);
+}
+
+promise.then(console.log, onRejected);
