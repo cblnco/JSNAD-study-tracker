8988319dc5e54077b81e1ab0062427a585da3539
Juan Carlos Blanco <juan.blanco.ornelas@ibm.com>
2020-10-07 18:33:01 -0500

    Updated comment on exec_find_wc.js example under child_process folder.

--- domains/child_processes/exec_find_wc.js
+++ domains/child_processes/exec_find_wc.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 // With exec we can use Shell syntax and then the output is stored in a buffer that we can handle in a callback.
-// This is the main difference between spawn (streams) and exec (Shell syntax and buffers).
+// This is the main difference between spawn (streams) and exec (buffers).
 // Better be used when we're expecting small results or amounts of data.
 exec('find . -type f | wc -l', (err, stdout, stderr) => {
    if (err) {
