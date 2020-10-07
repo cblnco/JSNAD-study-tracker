7b4aa1ed5d181496ba64c55fdf2328001d2bc0fb
Juan Carlos Blanco <juan.blanco.ornelas@ibm.com>
2020-10-07 18:31:16 -0500

    Added exec_find_wc.js example under child_process folder.

---
+++ domains/child_processes/exec_find_wc.js
@@ -0,0 +1,13 @@
+const { exec } = require('child_process');
+
+// With exec we can use Shell syntax and then the output is stored in a buffer that we can handle in a callback.
+// This is the main difference between spawn (streams) and exec (Shell syntax and buffers).
+// Better be used when we're expecting small results or amounts of data.
+exec('find . -type f | wc -l', (err, stdout, stderr) => {
+   if (err) {
+       console.error('An error happened.', err);
+       return;
+   }
+
+   return console.log(`Total number of files in the current directory: ${stdout}`);
+});
