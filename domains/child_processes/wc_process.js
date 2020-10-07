556d87eefeccae4d544c8d39fe3ceccbf4b4020e
Juan Carlos Blanco <juan.blanco.ornelas@ibm.com>
2020-10-07 18:04:10 -0500

    Added wc_process.js example under child_process folder.

--- domains/child_processes/wc_process.js
+++ domains/child_processes/wc_process.js
@@ -3,10 +3,15 @@ const { spawn } = require('child_process');
 let results = '';
 const child = spawn('wc');
 
+console.log('A child process started the execution of the \'wc\' command.');
+console.log('Write stuff, have fun and press Ctrl + d when you want to finish.');
+
 process.stdin.pipe(child.stdin);
 
 child.stdout.on('data', (chunk) => {
     results += chunk;
 });
 
-child.stdout.on('end', () => console.log("Results: ", results));
+child.stdout.on('end', () => {
+    console.log('Results:', results);
+});
