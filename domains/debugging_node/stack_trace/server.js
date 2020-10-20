3460d0325462f4a5ad154e5412a877a72e45812d
Juan Carlos Blanco <juan.blanco.ornelas@ibm.com>
2020-10-20 13:15:46 -0500

    Added stack_trace example

---
+++ domains/debugging_node/stack_trace/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes');
+const app = express();
+
+// Run server with --stack-trace-limit set to 21 to retain more stacks on each tick of the event loop.
+
+// We can also set a limitless stack trace using environment variables.
+/**
+ * if (process.env.NODE_ENV !== 'production') {
+ *  Error.stackTraceLimit = Infinity;
+ * }
+ */
+app.use(routes);
+app.listen(3000, () => console.log('Listening on port 3000.'));
+
